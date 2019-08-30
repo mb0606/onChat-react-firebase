cc9749a00b7c6cf0957f5f8e80dda86e50f681b6
mb0606 <marcoberardini@gmail.com>
2019-08-30 09:22:04 -0700

    Added Routing in index.js file with one Root to display App.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Setup routing
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Create Stateless functional component for routes
+const Root = () => {
+  <Router>
+    <Switch>
+      <Route path="/" components={App} />
+    </Switch>
+  </Router>;
+};
+
+// Render Root instead of App (App is route /)
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
