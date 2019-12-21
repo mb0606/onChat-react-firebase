611f18509e99c63d9864772be608051cdcbc0abb
mb0606 <marcoberardini@gmail.com>
2019-12-21 14:15:43 -0800

    Added formatCount to output plural or singular posts based on count.

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -15,6 +15,7 @@ class MetaPanel extends React.Component {
 		this.setState({ activeIndex: newIndex });
 
 	}
+	formatCount = count => (count > 1 || count === 0 ? `${count} posts` : `${count} post`)
 	displayTopPosters = post => (
 		Object.entries(post)
 			.sort((a, b) => b[1] - a[1])
@@ -23,7 +24,7 @@ class MetaPanel extends React.Component {
 					<Image avatar src={val.avatar} />
 					<List.Content>
 						<List.Header as="a">{key}</List.Header>
-						<List.Description>{val.count}</List.Description>
+						<List.Description>{this.formatCount(val.count)}</List.Description>
 					</List.Content>
 				</List.Item>
 			))
