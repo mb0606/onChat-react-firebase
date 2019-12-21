02966c9d65ac896c2c8e707fb85286b79561e14f
mb0606 <marcoberardini@gmail.com>
2019-12-21 15:01:32 -0800

    Added Modal to color panel.

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,8 +1,14 @@
 import React from "react"
-import { Sidebar, Menu, Divider, Button } from "semantic-ui-react";
+import { Sidebar, Menu, Divider, Button, Modal, Icon, Label } from "semantic-ui-react";
 
 class ColorPanel extends React.Component {
+    state = {
+        modal: false
+    }
+    openModal = () => this.setState({ modal: true })
+    closeModal = () => this.setState({ modal: false })
     render() {
+        const { modal } = this.state;
         return (
             <Sidebar
                 as={Menu}
@@ -13,7 +19,23 @@ class ColorPanel extends React.Component {
                 width="very thin"
             >
                 <Divider />
-                <Button icon="add" size="small" color="blue" />
+                <Button icon="add" size="small" color="blue" onClick={this.openModal} />
+
+                <Modal basic open={modal} onClose={this.closeModal}>
+                    <Modal.Header>Choose App Colors</Modal.Header>
+                    <Modal.Content>
+
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color="green" inverted>
+                            <Icon name="checkmark" /> Save Colors
+
+                        </Button>
+                        <Button color="red" inverted>
+                            <Icon name="remove" /> Cancel
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
             </Sidebar>
         )
     }
